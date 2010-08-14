43f0050a8490bf8c0ae13ece8bd47a4d18a6cd0d
Matti Schneider-Ghibaudo <alcmene.gs@gmail.com>
2010-08-14 10:29:21 +0200

    Now accepting options.

--- Source/Meio.Autocomplete.Geo.js
+++ Source/Meio.Autocomplete.Geo.js
@@ -6,7 +6,9 @@
 Meio.Autocomplete.Data.Geo = new Class({
 
 	Extends: Meio.Autocomplete.Data,
-
+	
+	geocoder: new google.maps.Geocoder(),
+	
 	/**Available options, from http://code.google.com/apis/maps/documentation/javascript/reference.html#GeocoderRequest
 	*	address		string			Address. Optional.
 	*	bounds		LatLngBounds	LatLngBounds within which to search. Optional.
@@ -14,19 +16,14 @@ Meio.Autocomplete.Data.Geo = new Class({
 	*	location	LatLng			LatLng about which to search. Optional.
 	*	region		string			Country code top-level domain within which to search. Optional.
 	*/
-	options: {
-	},
-	
-	geocoder: new google.maps.Geocoder(),
-	
-	initialize: function init(geocoderReqOpts, cache) {
+	initialize: function init(geocoderReqOpts) {
 		this._cache = new Meio.Autocomplete.Cache();
-		this.setOptions(geocoderReqOpts);
+		this.geocoderReqOpts = geocoderReqOpts; //we can't use Options because they change the prototype
 	},
 	
 	prepare: function(text){
 		this.cachedKey = text;
-		if (this._cache.has(this.cachedKey)){
+		if (this._cache.has(this.cachedKey)) {
 			this.data = this._cache.get(this.cachedKey);
 			this.fireEvent('ready');
 		} else {
@@ -35,8 +32,15 @@ Meio.Autocomplete.Data.Geo = new Class({
 	},
 	
 	search: function(query) {
+		var ro = this.geocoderReqOpts;
 		this.geocoder.geocode(
-			$merge(this.geocoderOpts, {address: query}),
+			{ //we can't use $merge because it changes the subobjects' prototypes
+				address:	query,
+				bounds:		ro.bounds,
+				language:	ro.language,
+				location:	ro.location,
+				region:		ro.region
+			},
 			this.handleResults.bind(this)
 		);
 	},
@@ -52,28 +56,28 @@ Meio.Autocomplete.Data.Geo = new Class({
 	    }
 	}
 });
-
-
+			
 Meio.Autocomplete.Geo = new Class({
 	Extends: Meio.Autocomplete,
-	
-	options: {
-		filter: {
-			path: 'formatted_address'
-		},
-		/**Available options, from http://code.google.com/apis/maps/documentation/javascript/reference.html#GeocoderRequest
-		*	address		string			Address. Optional.
-		*	bounds		LatLngBounds	LatLngBounds within which to search. Optional.
-		*	language	string			Preferred language for results. Optional.
-		*	location	LatLng			LatLng about which to search. Optional.
-		*	region		string			Country code top-level domain within which to search. Optional.
-		*/
-		geocoderRequest: {
-		}
+		
+	/**Available options, from http://code.google.com/apis/maps/documentation/javascript/reference.html#GeocoderRequest
+	*	bounds		LatLngBounds	LatLngBounds within which to search.
+	*	language	string			Preferred language for results.
+	*	location	LatLng			LatLng about which to search.
+	*	region		string			Country code top-level domain within which to search.
+	*/
+	initialize: function(input, geocoderRequestOptions, options, listInstance){
+		this.geocoderRequestOptions = geocoderRequestOptions; //we can't use Options because they change the prototype
+		options = $merge(options, {
+			filter: {
+				path: 'formatted_address'
+			}
+		});
+		this.parent(input, [], options, listInstance);
 	},
 	
 	initData: function() {
-		this.data = new Meio.Autocomplete.Data.Geo(this.options.geocoderRequest);
+		this.data = new Meio.Autocomplete.Data.Geo(this.geocoderRequestOptions);
 		this.data.addEvent('ready', this.dataReady.bind(this));
 	}
 });
