a009d2d95d0862a21b1766d29ddf29b7c0214ea3
Matti Schneider-Ghibaudo <alcmene.gs@gmail.com>
2010-08-14 08:28:19 +0200

    Functional version.

--- Source/Meio.Autocomplete.Geo.js
+++ Source/Meio.Autocomplete.Geo.js
@@ -6,17 +6,28 @@
 Meio.Autocomplete.Data.Geo = new Class({
 
 	Extends: Meio.Autocomplete.Data,
+
+	/**Available options, from http://code.google.com/apis/maps/documentation/javascript/reference.html#GeocoderRequest
+	*	address		string			Address. Optional.
+	*	bounds		LatLngBounds	LatLngBounds within which to search. Optional.
+	*	language	string			Preferred language for results. Optional.
+	*	location	LatLng			LatLng about which to search. Optional.
+	*	region		string			Country code top-level domain within which to search. Optional.
+	*/
+	options: {
+	},
 	
 	geocoder: new google.maps.Geocoder(),
 	
-	initialize: function init(geocoderOpts, cache) {
-		this._cache = cache;
-		this.geocoderOpts = geocoderOpts;
+	initialize: function init(geocoderReqOpts, cache) {
+		this._cache = new Meio.Autocomplete.Cache();
+		this.setOptions(geocoderReqOpts);
 	},
 	
 	prepare: function(text){
 		this.cachedKey = text;
 		if (this._cache.has(this.cachedKey)){
+			this.data = this._cache.get(this.cachedKey);
 			this.fireEvent('ready');
 		} else {
 			this.search(this.cachedKey);
@@ -31,8 +42,10 @@ Meio.Autocomplete.Data.Geo = new Class({
 	},
 	
 	handleResults: function(results, status) {
-	    if (status == google.maps.GeocoderStatus.OK) {
+		var gs = google.maps.GeocoderStatus; //better compression
+	    if (status == gs.OK || status == gs.ZERO_RESULTS) {
 			this.cache(this.cachedKey, results);
+			this.data = results;
 			this.fireEvent('ready');
 	    } else {
 			throw('Geocoding failed (status: "' + status + '").');
@@ -47,11 +60,20 @@ Meio.Autocomplete.Geo = new Class({
 	options: {
 		filter: {
 			path: 'formatted_address'
+		},
+		/**Available options, from http://code.google.com/apis/maps/documentation/javascript/reference.html#GeocoderRequest
+		*	address		string			Address. Optional.
+		*	bounds		LatLngBounds	LatLngBounds within which to search. Optional.
+		*	language	string			Preferred language for results. Optional.
+		*	location	LatLng			LatLng about which to search. Optional.
+		*	region		string			Country code top-level domain within which to search. Optional.
+		*/
+		geocoderRequest: {
 		}
 	},
 	
 	initData: function() {
-		this.data = new Meio.Autocomplete.Data.Geo({}, this.cache);
+		this.data = new Meio.Autocomplete.Data.Geo(this.options.geocoderRequest);
 		this.data.addEvent('ready', this.dataReady.bind(this));
 	}
 });
